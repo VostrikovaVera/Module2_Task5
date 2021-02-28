ed1b67abc7b139738d7e2c2c8372db02d0c92a2f
Vira Vostrikova <vvostrikova@csod.com>
2021-02-28 20:08:38 +0200

    Empty project added

---
+++ Module2_Task5/Module2_Task5/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Module2_Task5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
